6678d8b7c9ac1cd037c7aa0661dd210cca2d197b
fgh09101010 <fgh09101010@gmail.com>
2024-12-21 19:14:51 +0800

    (chore): 修改 requirements.txt and .gitignore

--- cryptocurrency/settings.py
+++ cryptocurrency/settings.py
@@ -16,7 +16,7 @@ import os
 from pathlib import Path
 
 
-env_path = Path(__file__).resolve().parents[2] / '.env'
+env_path = Path(__file__).resolve().parents[1] / '.env'
 load_dotenv(dotenv_path=env_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
