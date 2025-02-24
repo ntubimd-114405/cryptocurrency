93add64d4850db07307c3c3c0fe292ee7e1255c0
fgh09101010 <fgh09101010@gmail.com>
2025-02-24 12:20:53 +0800

    (refactor):重構view及templates

--- macro_trends/urls.py
+++ macro_trends/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import charts_view
+from .views import *
 
 urlpatterns = [
-    path('charts/', charts_view, name='charts'),
+    path('', macro_home, name='macro_home'),  # 將路徑連結到視圖
+    path('charts/', charts_view, name='macro_charts'),
 ]
