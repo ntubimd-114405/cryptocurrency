c39e1e0ccb8aab0cb15c31e0d2d1f382520f8c8f
fgh09101010 <fgh09101010@gmail.com>
2025-10-02 22:25:13 +0800

    (feat): 個人化報告頁面，新增新聞卡片樣式，移除不必要的報告部分，優化詞頻分析邏輯

--- report/urls.py
+++ report/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.report_list, name='weekly_report_list'),
     path('generate/', views.generate_weekly_report, name='generate_weekly_report'),
-    path('report/<int:report_id>/', views.view_weekly_report_by_id, name='view_weekly_report_by_id'),
+    path('<int:report_id>/', views.view_weekly_report_by_id, name='view_weekly_report_by_id'),
     path("chat/", views.chat_view, name="chat"),
     path("api/classify/", views.classify_question_api, name="classify_question_api"),
 ]
